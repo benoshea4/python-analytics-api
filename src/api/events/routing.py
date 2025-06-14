6c8273eefe13072b466157eb5381a05c4cc561fb
Admin <benoshea96@gmail.com>
2025-06-14 16:52:33 +0100

    Added compose

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,17 +1,51 @@
+import os
 from fastapi import APIRouter
-from .schemas import EventSchema
+from .schemas import (
+    EventSchema, 
+    EventListSchema, 
+    EventCreateSchema,
+    EventUpdateSchema
+    )
 
 router = APIRouter()
+from ..db.config import DB_URL
 
-# /api/events/
+# Get DATA Here
+# LIST VIEW
+# GET /api/events/
 @router.get("/")
-def read_events():
+def read_events() -> EventListSchema:
     # A bunch of items in a table
+    print(os.environ.get("DB_URL"), DB_URL)   
     return {
-        "results": [1,2,3]
+        "results": [{"id": 1}, {"id": 2}, {"id": 3}],
+        "count": 3
         }
 
+# SEND DATA HERE
+# CREATE VIEW
+# POST /api/events/
+@router.post("/")
+def create_event(payload:EventCreateSchema) -> EventSchema:
+    # A bunch of items in a table
+    data = payload.model_dump() # Payload -> dict -> pydantic
+    return {"id": 123, **data}
+
+# GET /api/events/12
 @router.get("/{event_id}")
 def get_event(event_id: int) -> EventSchema:
     # A single row
     return {"id": event_id}
+
+# Update this DATA 
+# PUT /api/events/12
+@router.put("/{event_id}")
+def update_event(event_id: int, payload:EventUpdateSchema) -> EventSchema:
+    # A single row
+    data = payload.model_dump()
+    return {"id": event_id, **data}
+
+#@router.delete("/{event_id}")
+# def delete_event(event_id: int) -> EventSchema:
+    # A single row
+    #return {"id": event_id}
